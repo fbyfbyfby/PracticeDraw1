7f630fb791f075d454c1753721da4677bb8a7967
bojan <fengboyang@sina.com>
2021-09-09 10:09:19 +0800

    practice4 finished

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice4DrawPointView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice4DrawPointView.java
@@ -2,11 +2,14 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice4DrawPointView extends View {
+    private Paint mPaint = new Paint();
 
     public Practice4DrawPointView(Context context) {
         super(context);
@@ -23,6 +26,12 @@ public class Practice4DrawPointView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(50);
+        canvas.drawPoint(200, 200, mPaint);
+        mPaint.setStrokeCap(Paint.Cap.SQUARE);
+        canvas.drawPoint(400, 200, mPaint);
 
 //        练习内容：使用 canvas.drawPoint() 方法画点
 //        一个圆点，一个方点
