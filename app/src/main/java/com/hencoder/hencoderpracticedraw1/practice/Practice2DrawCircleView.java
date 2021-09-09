bb1f7837e2201c927c2f369b7f02fe097574fbba
bojan <fengboyang@sina.com>
2021-09-09 09:59:16 +0800

    practice2 finished

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,11 +2,14 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice2DrawCircleView extends View {
+    private Paint mPaint = new Paint();
 
     public Practice2DrawCircleView(Context context) {
         super(context);
@@ -18,12 +21,23 @@ public class Practice2DrawCircleView extends View {
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        canvas.drawCircle((getWidth() / 6) * 2, getHeight() / 3 - 50, getWidth() / 6, mPaint);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(3f);
+        canvas.drawCircle((getWidth() / 6) * 4 + 50, getHeight() / 3 - 50, getWidth() / 6, mPaint);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.BLUE);
+        canvas.drawCircle((getWidth() / 6) * 2, (getHeight() / 3 - 50) * 3, getWidth() / 6, mPaint);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(10f);
+        mPaint.setColor(Color.BLACK);
+        canvas.drawCircle((getWidth() / 6)  * 4 + 50, (getHeight() / 3 - 50) * 3, getWidth() / 6, mPaint);
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
 
