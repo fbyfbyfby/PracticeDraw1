598d5958385746492137b874d35bc35d5fda3e33
bojan <fengboyang@sina.com>
2021-09-09 14:40:30 +0800

    practice9 finished

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,11 +2,18 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice9DrawPathView extends View {
+    private Paint mPaint = new Paint();
+    private Path mPath = new Path();
 
     public Practice9DrawPathView(Context context) {
         super(context);
@@ -20,9 +27,17 @@ public class Practice9DrawPathView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.BLACK);
+        mPath.addArc(100, 100, 200, 200, 160, 200);
+        mPath.arcTo(200, 100, 300, 200, 180, 200,false);
+        mPath.lineTo(200,250);
+        mPath.close();
+        canvas.drawPath(mPath, mPaint);
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
     }
