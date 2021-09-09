3ce04344679463149ec743751b3405e487cbdd15
bojan <fengboyang@sina.com>
2021-09-09 10:03:42 +0800

    practice3 finished

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
@@ -2,11 +2,13 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice3DrawRectView extends View {
+    private Paint mPaint = new Paint();
 
     public Practice3DrawRectView(Context context) {
         super(context);
@@ -23,7 +25,7 @@ public class Practice3DrawRectView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        canvas.drawRect((getWidth() / 3), getHeight() / 4, (getWidth() / 3) * 2, (getHeight() / 4) * 3, mPaint);
 //        练习内容：使用 canvas.drawRect() 方法画矩形
     }
 }
